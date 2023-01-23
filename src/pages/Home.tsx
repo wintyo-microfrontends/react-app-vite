d318fe2ec0283e48d20e520828a0634189556cfa
wintyo <wintyo1024@gmail.com>
2023-01-23 17:16:52 +0900

    静的ファイルを表示して、コンテナ側でも出せるか検証

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,8 @@ const Home: FC = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
+      <img src="/react-app/vite.svg" />
+      <br />
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
