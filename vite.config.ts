f1108f709ede756cf53e393c40400f3db9922289
wintyo <wintyo1024@gmail.com>
2023-01-23 18:14:38 +0900

    ライブラリ用ビルドと通常のビルドを切り替えられるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
+const SUB_DIRECTORY_NAME = "react-app";
+/** ライブラリ出力するか */
+const isLibMode = process.env.LIB_MODE === "true";
+
+console.log(process.env.NODE_ENV);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin()],
-  base: "/react-app/",
+  base: `/${SUB_DIRECTORY_NAME}/`,
   define: {
     "process.env.NODE_ENV": JSON.stringify(
       process.env.NODE_ENV || "development"
     ),
   },
-  build: {
-    lib: {
-      formats: ["es"],
-      entry: path.join(__dirname, "src/index.tsx"),
-      fileName: (format) => `index.${format}.js`,
-    },
-  },
+  build: isLibMode
+    ? {
+        lib: {
+          formats: ["es"],
+          entry: path.join(__dirname, "src/index.tsx"),
+          fileName: (format) => `index.${format}.js`,
+        },
+        outDir: path.join(__dirname, `dist-lib/${SUB_DIRECTORY_NAME}`),
+        manifest: true,
+        sourcemap: process.env.NODE_ENV !== "production" ? "inline" : false,
+        minify: process.env.NODE_ENV === "production",
+      }
+    : undefined,
 });
